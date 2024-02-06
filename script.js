e334887a05b01740d4bacad37c42a0cdba3e4add
611255008 <rylank7@educbe.ca>
2024-02-06 13:08:31 -0700

    e

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('Hello!');
+const button = document.getElementById('button');
+button.addEventListener('click', () => {
+handleSubmitform();
+});
+
+export function handleSubmitform() {
+    console.log('test')
+}
